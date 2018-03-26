820b19387b27e6feb2a413e844d6cfce19669082
Joy Paglinawan <virliejoyp@yahoo.com>
2018-03-25 22:22:38 -0700

    Add more questions to fill and button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,24 @@
-$(document).ready(event){
-  event.preventDefault
-  //var favorites = (function){
-    //$(favorites).enter(enter)
-    var inputs = document.getElementById('inputType').getElementsByTagName('input');
-var input = [];
-for (var i = 0, l = inputs.length; i < l; ++i) {
-    if (inputs[i].value.length) {
-        input.push(inputs[i].value);
-    }
-}
-  }
-}
+$(document).ready(function(){
+
+  $("text").submit(function(event) {
+  var favorites = ["fmovies", "author", "place", "painter"];
+  favorites.forEach(function(favorites){
+    favorites [1]
+    favorites[0]
+    favorites[3]
+
+  });
+
+  blanks.forEach(function(blank) {
+    var userInput = $("input" + blank).val();
+      $("." + blank).text(userInput);
+  });
+
+  $("text").show();
+
+  event.preventDefault();
+  });
+});
+  // var fthings = [];
+  // fthings.push("Raindrops on Roses")
+  // fthings;[]
