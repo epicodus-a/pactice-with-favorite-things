0f481ce746775dc6dab4accd60001f1ab196a263
Joy Paglinawan <virliejoyp@yahoo.com>
2018-03-25 23:41:52 -0700

    Add a working feature.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,36 @@
 $(document).ready(function(){
 
-  $("text").submit(function(event) {
-  var favorites = ["fmovies", "author", "place", "painter"];
-  favorites.forEach(function(favorites){
-    favorites [1]
-    favorites[0]
-    favorites[3]
-
-  });
-
-  blanks.forEach(function(blank) {
-    var userInput = $("input" + blank).val();
-      $("." + blank).text(userInput);
-  });
-
-  $("text").show();
-
+  $(".list").submit(function(event) {
   event.preventDefault();
+  var favThings =[] ;
+  var favMovies= $("#favMovies").val()
+  var favAuthor= $("#favAuthor").val()
+  var favAnimal= $("#favAnimal").val()
+  var favFood= $("#favFood").val()
+
+  favThings.push(favMovies)
+  favThings.push(favAuthor)
+  favThings.push(favAnimal)
+  favThings.push(favFood)
+  
+  $("#el1").text(function() {
+    return favThings[1];
+  })
+  $(".answer-list").show()
   });
-});
-  // var fthings = [];
-  // fthings.push("Raindrops on Roses")
-  // fthings;[]
+})
+
+
+  // blanks.forEach(function(blank) {
+  //   var userInput = $("input" + blank).val();
+  //     $("." + blank).text(userInput);
+  // });
+  //
+  // $("text").show();
+
+//     event.preventDefault();
+//   });
+// });
+  // var favThings = [];
+  // favThings.push("Raindrops on Roses")
+  // favThings;[]
