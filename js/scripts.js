567f85ced7de568970a71bba520d0d86e84b6f6c
Adriana C and Joy P <joy@>
2018-03-26 08:17:56 -0700

    Add append command, and made it work better,

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,24 @@ $(document).ready(function(){
   $(".list").submit(function(event) {
   event.preventDefault();
   var favThings =[] ;
-  var favMovies= $("#favMovies").val()
-  var favAuthor= $("#favAuthor").val()
-  var favAnimal= $("#favAnimal").val()
-  var favFood= $("#favFood").val()
+  var favMovies= $("#favMovies").val();
+  var favAuthor= $("#favAuthor").val();
+  var favAnimal= $("#favAnimal").val();
+  var favFood= $("#favFood").val();
 
-  favThings.push(favMovies)
-  favThings.push(favAuthor)
-  favThings.push(favAnimal)
-  favThings.push(favFood)
-  
-  $("#el1").text(function() {
-    return favThings[1];
-  })
+  favThings.push(favMovies);
+  favThings.push(favAuthor);
+  favThings.push(favAnimal);
+  favThings.push(favFood);
+  alert(favThings);
+
+  // $("#el1").text(function() {
+  //   return favThings[1];
+  // })
+  for(let i = 0; i < favThings.length; i ++){
+    let element = favThings[i];
+    $("ul").append('<li>'+element+'</li>')
+  }
   $(".answer-list").show()
   });
 })
